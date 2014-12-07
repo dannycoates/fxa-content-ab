34bef20636657d4422e5dada067dbe9fe4f90c00
Danny Coates <dannycoates@gmail.com>
2014-12-07 15:24:36 -0800

    new abatar api

--- index.js
+++ index.js
@@ -2,31 +2,11 @@ var AB = require('abatar')
 
 module.exports = AB.create(
   {
-    avatar: function (s) {
-      return /@mozilla\.(?:com|org)$/.test(s.email)
-    },
-    avatarText: function (s) {
-      return this.uniformChoice(
-        ['Change account picture','Change avatar'],
-        s.userId
-      )
-    },
-    foo: function (s) {
-      return this.bernoulliTrial(0.25, s.roll)
-    },
-    rando: function (s) {
-      var userId = s.userId
-      var hasBanner = this.bernoulliTrial(0.97, userId)
-      var condProb = hasBanner ? 0.95 : 0.5
-      var hasFeedStories = this.bernoulliTrial(condProb, userId)
-      var buttonText = this.uniformChoice(
-        ["I'm a voter", "I'm voting"],
-        userId
-      )
-      return {
-        hasBanner: hasBanner,
-        hasFeedStories: hasFeedStories,
-        buttonText: buttonText
+    enableAvatarLinkForMozillaEmails: {
+      subjectInputs: ['email'],
+      independentOutputs: ['avatarLinkVisible'],
+      groupingFunction: function (subject) {
+        return /@mozilla\.(?:com|org)$/.test(subject.email)
       }
     }
   }
